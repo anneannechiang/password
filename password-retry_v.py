fe12933625e3c0b0543a41be43d0c1d2a23b2458
Anne Chiang <anneannechiang@gmail.com>
2022-07-04 21:24:57 +0800

    change to while i > 0

---
+++ password-retry_v.py
@@ -0,0 +1,20 @@
+# 密碼重試程式
+
+# 預設密碼等於 a123456
+# 使用者【最多輸入3次密碼】
+# 不對的話, 顯示【密碼錯誤, 還有＿次機會】
+# 對的話, 顯示【登入成功！】
+
+
+password = 'a123456'
+i = 3
+
+while i > 0:
+	pw = input('Enter your password ' )
+	if pw == password:
+		print('Success Login!!')
+		break
+	else:
+		i = i - 1
+		print('Error password!', i ,'chance left! ')
+		
